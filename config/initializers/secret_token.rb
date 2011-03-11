dff2a79f8dd28399fada2e141845de0d1f25473c
Takeshi Sato <takeshi-sato@netyear.net>
2011-03-12 08:46:31 +0900

    install rails.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Japan::Application.config.secret_token = 'e6253688a6fa72e6d680c93fbe4d1e34a8902bedc9ea4b5b4f8eac3bb36839e29827cc2718dcb438602fae28e192a2bb003fff924845e4a6d60e12177b422d63'
